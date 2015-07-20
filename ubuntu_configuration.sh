00d2f7f161c3b076ee572fa38cf49734e4509358
Sanshan Gao <wangyigss@163.com>
2015-07-20 23:02:06 +0800

    Update ubuntu_configuration.sh

--- ubuntu_configuration.sh
+++ ubuntu_configuration.sh
@@ -1,7 +1,15 @@
 sudo apt-get update
 
+# install and configure ctags
+sudo apt-get install ctags
+wget -P ~ https://github.com/GSSBMW/just_configure_files/blob/master/.ctags
+
+# install and configure vim
+sudo apt-get install vim
+git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+wget -P ~ https://github.com/GSSBMW/just_configure_files/blob/master/.vimrc
+
 # install and configure Tmux
-pushd .; cd ~
 sudo apt-get install tmux
-wget https://github.com/GSSBMW/just_configure_files/blob/master/.tmux.conf
-popd
+wget -P ~ https://github.com/GSSBMW/just_configure_files/blob/master/.tmux.conf
+
